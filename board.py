a481eca334af72544b32adac80ab196df91a46c3
vmlr123 <vmlr123@gmail.com>
2022-07-18 21:13:12 -0400

    Board Printing Functionality added

--- board.py
+++ board.py
@@ -2,22 +2,29 @@
 # CSC 120 Section 0003
 # This is Tic Tac Toe
 
-board = ['-','-','-','-','-','-','-','-','-']
+board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+
 
 def print_board():
-  print(board[0],board[1],board[2])
-  print(board[3],board[4],board[5])
-  print(board[6],board[7],board[8])
+    print(board[0], board[1], board[2])
+    print(board[3], board[4], board[5])
+    print(board[6], board[7], board[8])
+
 
 def check_mark():
-  pass
+    pass
+
 
 def place_mark():
-  pass
+    pass
+
 
 def check_win():
-  
+    pass
+
+
 def main():
-  print('Welcome to Tic Tac Toe')
-  
+    print('Welcome to Tic Tac Toe')
+
+
 main()
