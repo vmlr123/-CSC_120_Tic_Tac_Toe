f4ee404b50140f93bfef2b7bc7999fc9430d25b4
vmlr123 <107290237+vmlr123@users.noreply.github.com>
2022-07-18 20:56:51 -0400

    Update board.py

--- board.py
+++ board.py
@@ -1,6 +1,23 @@
+# Created by Victor Lameda Rojas
+# CSC 120 Section 0003
+# This is Tic Tac Toe
+
 board = ['-','-','-','-','-','-','-','-','-']
 
-def prboard():
+def print_board():
   print(board[0],board[1],board[2])
   print(board[3],board[4],board[5])
   print(board[6],board[7],board[8])
+
+def check_mark():
+  pass
+
+def place_mark():
+  pass
+
+def check_win():
+  
+def main():
+  print('Welcome to Tic Tac Toe')
+  
+main()
