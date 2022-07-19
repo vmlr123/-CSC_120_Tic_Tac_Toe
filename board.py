55b7cfb68efef505403bae64f3d91f90790752a7
vmlr123 <vmlr123@gmail.com>
2022-07-18 22:30:39 -0400

    Alternate turns, invalid locations, and placing marks on the board added

--- board.py
+++ board.py
@@ -1,30 +1,52 @@
-# Created by Victor Lameda Rojas
-# CSC 120 Section 0003
-# This is Tic Tac Toe
+import time
 
-board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+# Initialize board
+board = {1: ' ', 2: ' ', 3: ' ',
+         4: ' ', 5: ' ', 6: ' ',
+         7: ' ', 8: ' ', 9: ' '}
+
+count = 0
+winner = False
+play = True
+tie = False
+current_player = ''
+player_details = []
+
+
+def get_player_details(currentplayer):
+    if currentplayer == 'A':
+        return ['B', 'O']
+    else:
+        return ['A', 'X']
 
 
 def print_board():
-    print(board[0], board[1], board[2])
-    print(board[3], board[4], board[5])
-    print(board[6], board[7], board[8])
+    for i in board:
+        print(i, ':', board[i], ' ', end='')
+        if i % 3 == 0:
+            print()
 
 
-def check_mark():
-    pass
 
 
-def place_mark():
-    pass
+
+def insert_input(slot_num, mark):
+    while board[slot_num] != ' ':
+        print("Mark cannot be placed in this spot")
+        slot_num = int(input())
+    board[slot_num] = mark
 
 
-def check_win():
-    pass
+while play:
 
+    print_board()
 
-def main():
-    print('Welcome to Tic Tac Toe')
+    player_details = get_player_details(current_player)
+    current_player = player_details[0]
+    print('The top row is 123, the middle row is 456, and the bottom row is 789')
+    print("Player {}: Enter a number between 1 and 9".format(current_player))
+    input_slot = int(input())
 
+    insert_input(input_slot, player_details[1])
+    count += 1
 
-main()
